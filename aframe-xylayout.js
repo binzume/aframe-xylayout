31aa590970a335f47c891b773abe2e082e1f80dc
Kousuke Kawahira <kawahira@binzume.net>
2019-10-14 16:18:48 +0900

    remove obsolete code.

--- aframe-xylayout.js
+++ aframe-xylayout.js
@@ -26,10 +26,12 @@ AFRAME.registerComponent('xycontainer', {
             return;
         }
         let isVertical = data.direction === "vertical" || data.direction === "column";
+        let padding = data.padding;
+        let spacing = data.spacing;
         let mainDir = (data.reverse ^ isVertical) ? -1 : 1;
         let xymat = isVertical ? [0, 1, mainDir, 0] : [mainDir, 0, 0, -1]; // [main,corss] to [x,y]
         let xyToMainCross = isVertical ? (x, y) => [y, x] : (x, y) => [x, y];
-        let containerSize = xyToMainCross(containerRect.width - data.padding * 2, containerRect.height - data.padding * 2);
+        let containerSize = xyToMainCross(containerRect.width - padding * 2, containerRect.height - padding * 2);
         let attrNames = xyToMainCross("width", "height");
 
         // lines
@@ -61,11 +63,11 @@ AFRAME.registerComponent('xycontainer', {
             if (itemData.size[0] == null || isNaN(itemData.size[0])) {
                 continue;
             }
-            let sz = sizeSum + itemData.size[0] * itemData.scale[0] + data.spacing * targets.length;
+            let sz = sizeSum + itemData.size[0] * itemData.scale[0] + spacing * targets.length;
             if (data.wrap == "wrap" && sizeSum > 0 && sz > containerSize[0]) {
                 lines.push({ targets: targets, sizeSum: sizeSum, growSum: growSum, shrinkSum: shrinkSum, crossSize: crossSize });
                 crossSizeSum += crossSize;
-                mainSize = Math.max(mainSize, sizeSum + data.spacing * (lines.length - 1));
+                mainSize = Math.max(mainSize, sizeSum + spacing * (lines.length - 1));
                 targets = [];
                 sizeSum = 0;
                 growSum = 0;
@@ -80,18 +82,18 @@ AFRAME.registerComponent('xycontainer', {
         }
         if (targets.length > 0) {
             lines.push({ targets: targets, sizeSum: sizeSum, growSum: growSum, shrinkSum: shrinkSum, crossSize: crossSize });
-            mainSize = Math.max(mainSize, sizeSum + data.spacing * (lines.length - 1));
+            mainSize = Math.max(mainSize, sizeSum + spacing * (lines.length - 1));
             crossSizeSum += crossSize;
         }
 
-        crossSizeSum += data.spacing * (lines.length - 1);
+        crossSizeSum += spacing * (lines.length - 1);
         if (containerRect.data[attrNames[0]] == -1) {
             containerSize[0] = mainSize;
-            containerRect[attrNames[0]] = mainSize + data.padding * 2;
+            containerRect[attrNames[0]] = mainSize + padding * 2;
         }
         if (containerRect.data[attrNames[1]] == -1) {
             containerSize[1] = crossSizeSum;
-            containerRect[attrNames[1]] = crossSizeSum + data.padding * 2;
+            containerRect[attrNames[1]] = crossSizeSum + padding * 2;
         }
         let containerPivot = xyToMainCross(containerRect.data.pivotX, containerRect.data.pivotY);
         let crossOffset = -containerPivot[1] * containerSize[1];
@@ -107,29 +109,27 @@ AFRAME.registerComponent('xycontainer', {
         }
         lines.forEach(l => {
             containerSize[1] = l.crossSize + crossStretch;
-            this._layoutLine(l.targets, l.sizeSum, l.growSum, l.shrinkSum, containerSize, mainOffset, crossOffset, xymat, attrNames);
-            crossOffset += containerSize[1] + data.spacing;
+            this._layoutLine(l.targets, l.sizeSum, l.growSum, l.shrinkSum, mainOffset, crossOffset, containerSize, xymat, attrNames);
+            crossOffset += containerSize[1] + spacing;
         });
     },
-    _layoutLine(targets, sizeSum, growSum, shrinkSum, containerSize, p, crossOffset, xymat, attrNames) {
-        let data = this.data;
-        let spacing = data.spacing;
-        let justify = data.justifyItems;
+    _layoutLine(targets, sizeSum, growSum, shrinkSum, p, crossOffset, containerSize, xymat, attrNames) {
+        let { justifyItems, alignItems, spacing } = this.data;
         let stretchFactor = 0;
-        if (justify === "center") {
+        if (justifyItems === "center") {
             p += (containerSize[0] - sizeSum - spacing * targets.length) / 2;
-        } else if (justify === "end") {
+        } else if (justifyItems === "end") {
             p += (containerSize[0] - sizeSum - spacing * targets.length);
-        } else if (justify === "stretch") {
+        } else if (justifyItems === "stretch") {
             stretchFactor = containerSize[0] - sizeSum - spacing * (targets.length - 1);
             if (stretchFactor > 0) {
                 stretchFactor = growSum > 0 ? stretchFactor / growSum : 0;
             } else {
                 stretchFactor = shrinkSum > 0 ? stretchFactor / shrinkSum : 0;
             }
-        } else if (justify === "space-between") {
+        } else if (justifyItems === "space-between") {
             spacing = (containerSize[0] - sizeSum) / (targets.length - 1);
-        } else if (justify === "space-around") {
+        } else if (justifyItems === "space-around") {
             spacing = (containerSize[0] - sizeSum) / targets.length;
             p += spacing * 0.5;
         }
@@ -138,7 +138,7 @@ AFRAME.registerComponent('xycontainer', {
             let itemData = targets[i];
             let item = itemData.el;
             let layoutItem = item.getAttribute('xyitem');
-            let align = (layoutItem && layoutItem.align) || data.alignItems;
+            let align = (layoutItem && layoutItem.align) || alignItems;
             let stretch = (layoutItem ? (stretchFactor > 0 ? layoutItem.grow : layoutItem.shrink) : 1) * stretchFactor;
             let szMain = itemData.size[0] * itemData.scale[0] + stretch;
             let szCross = itemData.size[1];
@@ -168,7 +168,9 @@ AFRAME.registerComponent('xycontainer', {
         }
     },
     requestLayout() {
-        this.data && this._layout(this.el.components.xyrect, this.el.children);
+        if (this.data) {
+            this._layout(this.el.components.xyrect, this.el.children);
+        }
     }
 });
 
@@ -180,8 +182,11 @@ AFRAME.registerComponent('xyitem', {
         fixed: { default: false }
     },
     update(oldData) {
-        if (oldData.align !== undefined && this.el.parent.components.xycontainer) {
-            this.el.parent.components.xycontainer.requestLayout();
+        if (oldData.align !== undefined) {
+            let xycontainer = this.el.parent.components.xycontainer;
+            if (xycontainer) {
+                xycontainer.requestLayout();
+            }
         }
     }
 });
@@ -194,50 +199,40 @@ AFRAME.registerComponent('xyrect', {
         pivotY: { default: 0.5 }
     },
     update(oldData) {
-        let data = this.data;
-        let geometry = this.el.getAttribute("geometry") || {};
+        let data = this.data, el = this.el;
+        let geometry = el.getAttribute("geometry") || {};
         let w = data.width, h = data.height;
         if (w < 0) {
-            w = this.el.hasAttribute("width") ? this.el.getAttribute("width") * 1 : geometry.width || 0;
-            // w = geometry ? geometry.width : this.el.getAttribute("width") * 1 || 0;
+            w = el.hasAttribute("width") ? el.getAttribute("width") * 1 : geometry.width || 0;
+            // w = geometry ? geometry.width : el.getAttribute("width") * 1 || 0;
         }
         if (h < 0) {
-            h = this.el.hasAttribute("height") ? this.el.getAttribute("height") * 1 : geometry.height || 0;
-            // h = geometry ? geometry.height : this.el.getAttribute("height") * 1 || 0;
+            h = el.hasAttribute("height") ? el.getAttribute("height") * 1 : geometry.height || 0;
+            // h = geometry ? geometry.height : el.getAttribute("height") * 1 || 0;
         }
         this.width = w;
         this.height = h;
 
-        if (this.el.components.rounded || this.el.tagName == "A-INPUT") {
-            // hack for a-frame-material
-            data.pivotX = 0;
-            data.pivotY = 1;
-        }
-
         if (oldData.width !== undefined) {
-            this.el.emit('xyresize', { xyrect: this }, false);
+            el.emit('xyresize', { xyrect: this }, false);
         }
     }
 });
 
-(function () {
-    let schema = {
-        defaultComponents: {
-            xyrect: {},
-            xycontainer: {}
-        },
-        mappings: {
-            width: 'xyrect.width',
-            height: 'xyrect.height',
-            direction: 'xycontainer.direction',
-            spacing: 'xycontainer.spacing',
-            padding: 'xycontainer.padding',
-            reverse: 'xycontainer.reverse',
-            wrap: 'xycontainer.wrap',
-            "align-items": 'xycontainer.alignItems',
-            "justify-items": 'xycontainer.justifyItems'
-        }
-    };
-    AFRAME.registerPrimitive('a-xycontainer', schema);
-    AFRAME.registerPrimitive('a-xylayout', schema); // deprecated
-})();
+AFRAME.registerPrimitive('a-xycontainer', {
+    defaultComponents: {
+        xyrect: {},
+        xycontainer: {}
+    },
+    mappings: {
+        width: 'xyrect.width',
+        height: 'xyrect.height',
+        direction: 'xycontainer.direction',
+        spacing: 'xycontainer.spacing',
+        padding: 'xycontainer.padding',
+        reverse: 'xycontainer.reverse',
+        wrap: 'xycontainer.wrap',
+        "align-items": 'xycontainer.alignItems',
+        "justify-items": 'xycontainer.justifyItems'
+    }
+});
