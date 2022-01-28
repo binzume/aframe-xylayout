d63165acebe2743321101628c37b766a0e02a82e
Kousuke Kawahira <kawahira@binzume.net>
2022-01-28 19:32:23 +0900

    add resizing rectangle.

--- experimental/xyresize.js
+++ experimental/xyresize.js
@@ -42,17 +42,39 @@ AFRAME.registerComponent('xyresize', {
 			resizerEl.setAttribute('visible', data.visibleAlways);
 		});
 		resizerEl.setAttribute('xydraggable', { base: el });
+		/**
+		 * @type {THREE.LineLoop}
+		 */
+		let rectObj = null;
 		resizerEl.addEventListener('xy-drag', ev => {
+			if (!rectObj) {
+				const material = new THREE.LineBasicMaterial({ color: 0x8888ff });
+				const points = [
+					new THREE.Vector3(-0.5, -0.5, 0),
+					new THREE.Vector3(-0.5, 0.5, 0),
+					new THREE.Vector3(0.5, 0.5, 0),
+					new THREE.Vector3(0.5, -0.5, 0),
+				];
+				rectObj = new THREE.LineLoop(new THREE.BufferGeometry().setFromPoints(points), material);
+				el.setObject3D('resizing', rectObj);
+			}
+			let w = Math.max(ev.detail.point.x * 2, data.minWidth);
+			let h = Math.max(-ev.detail.point.y * 2, data.minHeight);
+			rectObj.scale.set(w, h, 1);
 			resizerEl.setAttribute('position', { x: ev.detail.point.x, y: ev.detail.point.y, z: 0.01 });
 		});
 		resizerEl.addEventListener('xy-dragend', ev => {
-			let rect = el.components.xyrect;
-			let w = Math.max(ev.detail.point.x + rect.width / 2, data.minWidth);
-			let h = Math.max(-ev.detail.point.y + rect.height / 2, data.minHeight);
-			let dw = w - rect.width;
-			let dh = h - rect.height;
+			if (rectObj) {
+				el.removeObject3D('resizing');
+				rectObj.geometry.dispose();
+				if (rectObj.material instanceof THREE.Material) {
+					rectObj.material.dispose();
+				}
+			}
+			rectObj = null;
+			let w = Math.max(ev.detail.point.x * 2, data.minWidth);
+			let h = Math.max(-ev.detail.point.y * 2, data.minHeight);
 			el.setAttribute('xyrect', { width: w, height: h });
-			el.object3D.position.add(new THREE.Vector3(dw / 2, -dh / 2, 0).multiply(el.object3D.scale));
 		});
 
 		this._onResized = this._onResized.bind(this);
@@ -62,7 +84,7 @@ AFRAME.registerComponent('xyresize', {
 		this._onResized();
 	},
 	_onResized() {
-		let d = 0.1;
+		let d = 0.2;
 		let rect = this.el.components.xyrect;
 		this._resizerEl.setAttribute('position', { x: rect.width / 2 + d, y: -rect.height / 2 - d, z: 0.01 });
 	},
